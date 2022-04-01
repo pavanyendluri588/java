258e85b5346e369bca21f39bb33c285b09253708
pavanyendluri588@ <pavanyendluri588@gmail.com>
2022-04-02 01:41:33 +0530

    april_2nd_upload

--- touch.sh
+++ touch.sh
@@ -1,6 +1,6 @@
-igit init
+git init
 git remote add origin https://github.com/Pavanramchandar/java.git
 git remote -v
 git add . --force
-git commit -m "april_upload" 
+git commit -m "april_2nd_upload" 
 git push origin master --force
